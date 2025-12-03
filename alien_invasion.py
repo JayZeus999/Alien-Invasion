0a386f1dd26ebed5b97466b414b1710af8ec2bd1
Falade Joseph <faladejoseph400@gmail.com>
2025-12-03 19:25:51 +0100

    Create Clock instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,6 +12,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.clock = pygame.time.Clock()
+        
+
 
     def run_game(self):
             """Start the main loop for the game"""
@@ -23,6 +26,7 @@ class AlienInvasion:
 
                 #Make the most recently drawn screen visible.
                 pygame.display.flip()
+                self.clock.tick(60)
 
 
 if __name__ == "__main__":
