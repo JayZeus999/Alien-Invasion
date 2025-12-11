89d20dc04f7c98226708387d9cceb60e213f0581
Falade Joseph <faladejoseph400@gmail.com>
2025-12-11 18:06:39 +0100

    Add continous right press

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,12 +20,15 @@ class AlienInvasion:
         self.ship = Ship(self)
         self.character = Character(self)
 
+
     def run_game(self):
             """Start the main loop for the game"""
             while True:
                 self._check_events()
+                self.ship.update()
                 self._update_screen()
                 self.clock.tick(60)
+
 
     def _check_events(self):
         """Respond to keypresses & mouse events."""
@@ -33,9 +36,17 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            # Move the ship to the right.
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+
+            # Halt the ship once finger is lifted.
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
@@ -46,6 +57,7 @@ class AlienInvasion:
         self.character.blitme()
         #Make the most recently drawn screen visible.
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
