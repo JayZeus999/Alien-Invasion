5e721d18578481ae189341edeb28f6c738551f8a
Falade Joseph <faladejoseph400@gmail.com>
2025-12-11 17:37:02 +0100

    Add right key function to Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
+from character import Character
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -15,23 +17,35 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-
+        self.ship = Ship(self)
+        self.character = Character(self)
 
     def run_game(self):
             """Start the main loop for the game"""
             while True:
-                # Watch for keyboard and mouse events.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
-
-                # Redraw the screen during each pass through the loop
-                self.screen.fill(self.settings.bg_color)
-                #Make the most recently drawn screen visible.
-                pygame.display.flip()
+                self._check_events()
+                self._update_screen()
                 self.clock.tick(60)
 
+    def _check_events(self):
+        """Respond to keypresses & mouse events."""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # self.ship.fill(self.ship.bg_color)
+        self.character.blitme()
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == "__main__":
     # Make a game instance, and run the game.
--- character.py
+++ character.py
@@ -1,22 +1,21 @@
 import pygame
 
-class Ship:
+class Character:
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Initialize the ship and set its starting position."""
+        """Initialize the character & her starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-        self.bg_color = (0, 0, 230)
 
         # Load the ship image & get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("images/female_bard.png")
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom 
+        self.rect.center = self.screen_rect.center
 
     
     def blitme(self):
-        """Draw the ship at its current location."""
+        """Draw the character at its current location."""
         self.screen.blit(self.image, self.rect)
