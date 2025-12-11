89ec801492ec6ad86ecc676a6f66cda2ab46f999
Falade Joseph <faladejoseph400@gmail.com>
2025-12-11 18:27:40 +0100

    Add continous left press to Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,10 +42,16 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
 
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
             # Halt the ship once finger is lifted.
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
 
     def _update_screen(self):
