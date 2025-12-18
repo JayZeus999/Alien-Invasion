ffc39719ca82031fda27afe95a5d75b266ae973c
Falade Joseph <faladejoseph400@gmail.com>
2025-12-18 15:59:03 +0100

    Create bullet class

--- settings.py
+++ settings.py
@@ -8,4 +8,9 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (0, 0, 0)
         self.ship_speed = 1.5
-        self.rocket_speed = 2.5
+
+        # Bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
