c020cec2c132a9a970223d9e238665750054f15f
Falade Joseph <faladejoseph400@gmail.com>
2025-12-18 17:24:46 +0100

    Delete used bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,12 @@ class AlienInvasion:
                 self._update_screen()
                 self.clock.tick(60)
 
+                # Get rid of bullets that have disappeared.
+                for bullet in self.bullets.copy():
+                    if bullet.rect.bottom <= 0:
+                        self.bullets.remove(bullet)
+                print(len(self.bullets))
+
 
     def _check_events(self):
         """Respond to keypresses & mouse events."""
