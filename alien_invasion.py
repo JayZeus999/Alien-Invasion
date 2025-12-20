729ca8bddd31fafea8ce13c3c4f8e3cfce0addc8
Falade Joseph <faladejoseph400@gmail.com>
2025-12-20 17:39:28 +0100

    Add a update mrthod for aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,8 +5,6 @@ from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
-from star import Star
-from random import randint
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -26,11 +24,9 @@ class AlienInvasion:
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
-        # self.aliens = pygame.sprite.Group()
-        self.stars = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
 
-        # self._create_fleet()
-        self._create_constellation()
+        self._create_fleet()
 
 
     def run_game(self):
@@ -39,6 +35,7 @@ class AlienInvasion:
                 self._check_events()
                 self.ship.update()
                 self._update_bullets()
+                self._update_aliens()
                 self._update_screen()
                 self.clock.tick(60)
 
@@ -93,57 +90,30 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
-    # def _create_fleet(self):
-    #     """Create a fleet of aliens."""
-    #     # Create an alien and keep adding aliens until there's no room left.
-    #     # Spacing between aliens is one alien width.
-    #     alien = Alien(self)
-    #     alien_width, alien_height = alien.rect.size
+    def _create_fleet(self):
+        """Create a fleet of aliens."""
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
 
-    #     current_x, current_y = alien_width, alien_height
-    #     while current_y < (self.settings.screen_height - 3 * alien_height):
-    #         while current_x < (self.settings.screen_width - 2 * alien_width):
-    #             self._create_alien(current_x, current_y)
-    #             current_x += 2 * alien_width
-                
-    #         # Finished a row; reset x value, and increment y value.
-    #         current_x = alien_width
-    #         current_y += 2 * alien_height
-
-
-    # def _create_alien(self, x_position, y_position):
-    #     new_alien = Alien(self)
-    #     new_alien.x = x_position
-    #     new_alien.rect.x = x_position
-    #     new_alien.rect.y = y_position
-    #     self.aliens.add(new_alien)
-
-
-    def _create_constellation(self):
-        """Create a constellation of aliens."""
-        # Create a star and keep adding stars until there's no room left.
-        # Spacing between stars is one star width.
-        star = Star(self)
-        star_width, star_height = star.rect.size
-
-        current_x, current_y = star_width, star_height
-        random_number = randint(-10, 10)
-        while current_y < (self.settings.screen_height - random_number):
-            while current_x < (self.settings.screen_width - random_number):
-                self._create_star(current_x, current_y)
-                current_x += 2 * star_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
                 
             # Finished a row; reset x value, and increment y value.
-            current_x = star_width
-            current_y += 2 * star_height
+            current_x = alien_width
+            current_y += 2 * alien_height
 
 
-    def _create_star(self, x_position, y_position):
-        new_star = Star(self)
-        new_star.x = x_position
-        new_star.rect.x = x_position
-        new_star.rect.y = y_position
-        self.stars.add(new_star)
+    def _create_alien(self, x_position, y_position):
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
 
     def _update_bullets(self):
@@ -157,6 +127,11 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
 
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
+
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop
@@ -166,8 +141,7 @@ class AlienInvasion:
             bullet.draw_bullet()
 
         self.ship.blitme()
-        # self.aliens.draw(self.screen)
-        self.stars.draw(self.screen)
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
